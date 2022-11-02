00a41dbf11e331c31f68e94191f04816c350ec4c
koudyk <kendra.oudyk@gmail.com>
2022-11-02 19:55:00 -0400

    get index_info.py to work by ignoring jerome's old labels (wrong format)

--- index_info.py
+++ index_info.py
@@ -1,5 +1,3 @@
-import json
-import ast
 from pathlib import Path
 
 import pandas as pd
@@ -12,37 +10,10 @@ pmids_and_pmcids = pd.read_csv(constants.PATH_TO_OPEN_FMRI_PMIDS_AND_PMCIDS)
 
 
 def get_documents(project_path):
-    all_docs = {}
+    counter = -1
     docs_folder = project_path / "documents"
     for doc_path in docs_folder.glob("*.jsonl"):
-        j_df = pd.read_json(doc_path, lines=True)
-        if "pmcid" in j_df.iloc[0]["metadata"].keys():
-            for i, row in j_df.iterrows():
-                all_docs[row["metadata"]["pmcid"]] = row
-        else:
-            for i, row in j_df.iterrows():
-                pmid = row["metadata"]["pmid"]
-                pmcid = int(pmids_and_pmcids[pmids_and_pmcids["pmid"] == pmid]["pmcid"])
-                row["metadata"]["pmcid"] = pmcid
-                all_docs[row["metadata"]["pmcid"]] = row
-    df = pd.DataFrame.from_dict(all_docs, orient="index")
-    df.index = df.index.set_names(["pmcid"])
-    df.reset_index(inplace=True)
-    df["docs_from_project"] = project_path.name
-    return df
-
-
-def get_annotations(project_path):
-    all_anns = {}
-    anns_folder = project_path / "annotations"
-    ann_counts = []
-    counter = -1
-    for ann_path in anns_folder.glob("*.json*"):
-        if str(ann_path)[-1] == "l":
-            df = pd.read_json(ann_path, lines=True)
-        else:
-            df = pd.read_json(ann_path)
-
+        df = pd.read_json(doc_path, lines=True)
         if not "pmcid" in df.iloc[0]["metadata"].keys():
             for i, row in df.iterrows():
                 pmid = row["metadata"]["pmid"]
@@ -52,59 +23,83 @@ def get_annotations(project_path):
         for i, row in df.iterrows():
             counter = counter + 1
             df.at[counter, "pmcid"] = row["metadata"]["pmcid"]
-            ann_counts.append(len(row["annotations"]))
-        df["annotation_file"] = ann_path.name
+        df["document_file"] = doc_path.name
 
-    # df = pd.DataFrame.from_dict(all_anns, orient="index")
-    # df.index = df.index.set_names(["pmcid"])
-    # df.reset_index(inplace=True)
-    df["annotations_from_project"] = project_path.name
+    df["document_from_project"] = project_path.name
+    return df
 
-    return df, ann_counts
+
+def get_annotations(project_path):
+    anns_folder = project_path / "annotations"
+    ann_count = 0
+    counter = -1
+    for ann_path in anns_folder.glob("*.json*"):
+        if not ann_path.name == "labelled_texts_jerome.jsonl":
+            if str(ann_path)[-1] == "l":
+                df = pd.read_json(ann_path, lines=True)
+            else:
+                df = pd.read_json(ann_path)
+
+            if not "pmcid" in df.iloc[0]["metadata"].keys():
+                for i, row in df.iterrows():
+                    pmid = row["metadata"]["pmid"]
+                    pmcid = int(
+                        pmids_and_pmcids[pmids_and_pmcids["pmid"] == pmid]["pmcid"]
+                    )
+                    df.at[i, "metadata"]["pmcid"] = pmcid
+
+            for i, row in df.iterrows():
+                counter = counter + 1
+                df.at[counter, "pmcid"] = row["metadata"]["pmcid"]
+                ann_count = ann_count + len(row["annotations"])
+            df["annotation_file"] = ann_path.name
+    df["annotation_project"] = project_path.name
+
+    return df, ann_count
 
 
 def get_project_data(project_path):
-    anns, ann_counts = get_annotations(project_path)
+    anns, ann_count = get_annotations(project_path)
     docs = get_documents(project_path)
     df = pd.merge(anns, docs, how="inner", on="pmcid")
-    print(len(df), project_path.name)
-    # df.dropna(axis='index', subset=['annotations'], inplace=True)
-
-    # check if we've matched the docs and annotations by
-    # checking if the metadata is the same in the annotations & documents
-    # assert df["metadata_x"].equals(df["metadata_y"])
+    df.dropna(axis="index", subset=["annotations"], inplace=True)
 
     # add the metadata in columns of their own instead of a nested dict
-    metadata = pd.json_normalize(df["metadata_x"])
+    metadata = pd.json_normalize(df["metadata_y"])
     df = pd.merge(df, metadata, how="inner", on="pmcid")
-    df.drop(labels=["metadata_x", "metadata_y"], axis="columns", inplace=True)
-    return df, ann_counts
+
+    # check if we've matched the docs and annotations
+    assert df["metadata_x"].equals(df["metadata_y"])
+    return df, ann_count
 
 
-total_ann_count = []
+total_ann_count = 0
 for i_project, project_path in enumerate(projects_list):
     if i_project == 0:
-        df_dense, ann_counts1 = get_project_data(project_path)
+        df_dense, ann_count = get_project_data(project_path)
     else:
-        df_temp, ann_counts2 = get_project_data(project_path)
+        df_temp, ann_count = get_project_data(project_path)
         df_dense = pd.concat([df_dense, df_temp])
-    # total_ann_count = total_ann_count + ann_counts
+
+    total_ann_count = total_ann_count + ann_count
 
 # expand df so there's one row per annotation
 dense_cols = list(df_dense.columns)
 dense_cols.remove("text")
-cols = dense_cols + ["annotated_text", "annotated_sentence", "annotated_paragraph"]
-################## there's something wrong with the total_ann_count
-# df = pd.DataFrame(index=range(total_ann_count), columns=cols)
-df = pd.DataFrame(columns=cols)
+cols = dense_cols + [
+    "annotation_label_name",
+    "annotated_text",
+    "annotated_sentence",
+    "annotated_paragraph",
+]
+
+df = pd.DataFrame(index=range(total_ann_count), columns=cols)
 i = -1
-is_ = []
 for i_row_dense, row in df_dense.iterrows():
     text = row["text"]
     for ann in row["annotations"]:
         i = i + 1
-        is_.append(i)
-
+        df.loc[i, "annotation_label_name"] = ann["label_name"]
         df.loc[i, "annotated_text"] = text[ann["start_char"] : ann["end_char"]]
 
         i_start_sentence = text[: ann["start_char"]].rfind(". ") + 1
@@ -122,11 +117,24 @@ for i_row_dense, row in df_dense.iterrows():
 
         df.loc[i, dense_cols] = row[dense_cols]
 
-        # surrounding_sentence
+assert df["utf8_text_md5_checksum_x"].equals(df["utf8_text_md5_checksum_y"])
 
-import matplotlib.pyplot as plt
+# remake dataframe so it's more understandable
+df["document_utf8_text_md5_checksum"] = df["utf8_text_md5_checksum"]
+df["labelbuddy_display_title"] = df["display_title"]
 
-plt.plot(is_)
-
-
-x = 1
+df_useful_cols = [
+    "pmcid",
+    "doi",
+    "document_utf8_text_md5_checksum",
+    "document_file",
+    "document_from_project",
+    "annotation_label_name",
+    "annotated_text",
+    "annotated_sentence",
+    "annotated_paragraph",
+    "annotation_file",
+    "annotation_project",
+    "labelbuddy_display_title",
+]
+df = df[df_useful_cols]
