bc0dd0c54727804690691d81c152e51f1c2c0aaf
Jerome Dockes <jerome@dockes.org>
2022-11-16 09:41:10 -0500

    remove participants_demographics dependency

--- analysis/book/plot_n_participants.py
+++ analysis/book/plot_n_participants.py
@@ -12,6 +12,109 @@
 #     name: python3
 # ---
 
+# The first part is just defining a function for converting text to numbers,
+# for example "twenty-one" → 21. You can skip over it as it is not very
+# relevant to the rest of this notebook.
+
+# +
+import re
+
+import lark
+
+numbers_grammar = r"""
+number: NUMBER | text_number
+
+?text_number.1: hundred_text_number | dozen_text_number | TEEN_NAME | UNIT_NAME
+
+
+hundred_text_number.1: (dozen_text_number | UNIT_NAME) "hundred" [ "and" ] [ dozen_text_number | UNIT_NAME ]
+
+dozen_text_number.1: DOZEN_NAME [ UNIT_NAME ] | TEEN_NAME
+
+UNIT_NAME.1: "one"|"two"|"three"|"four"|"five"|"six"|"seven"|"eight"|"nine"
+
+TEEN_NAME.1: "ten"|"eleven"|"twelve"|"thirteen"|"fourteen"|"fifteen"
+    |"sixteen"|"seventeen"|"eighteen"|"nineteen"
+
+DOZEN_NAME.1: "twenty"|"thirty"|/fou?rty/|"fifty"|"sixty"|"seventy"|"eighty"
+    |"ninety"
+
+
+WS: /\s+/
+PUNCT: /[^a-zA-Z0-9().]/
+
+%import common (NUMBER)
+%ignore WS
+%ignore PUNCT
+"""
+
+
+class NumberTransformer(lark.Transformer):
+    def number(self, tree) -> int:
+        return tree[0]
+
+    def hundred_text_number(self, tree) -> int:
+        hundred_part, rest = tree
+        if rest is None:
+            return 100 * hundred_part
+        return 100 * hundred_part + rest
+
+    def dozen_text_number(self, tree) -> int:
+        if len(tree) == 1:
+            teen = tree[0]
+            return teen
+        dozen, unit = tree
+        if unit is None:
+            return dozen
+        return dozen + unit
+
+    def UNIT_NAME(self, tree) -> int:
+        return (
+            "zero one two three four five six seven eight nine".split().index(
+                tree.value.lower()
+            )
+        )
+
+    def TEEN_NAME(self, tree) -> int:
+        return (
+            "ten eleven twelve thirteen fourteen fifteen sixteen "
+            "seventeen eighteen nineteen".split().index(tree.value.lower())
+            + 10
+        )
+
+    def DOZEN_NAME(self, tree) -> int:
+        return (
+            "zero ten twenty thirty forty fifty sixty "
+            "seventy eighty ninety".split().index(
+                tree.value.lower().replace("fourty", "forty")
+            )
+        ) * 10
+
+    def NUMBER(self, tree) -> int:
+        return int(tree.value)
+
+
+class TextToNumber:
+    def __init__(self):
+        self.parser = lark.Lark(
+            numbers_grammar,
+            ambiguity="resolve",
+            start="number",
+            g_regex_flags=re.I,
+        )
+        self.transformer = NumberTransformer()
+
+    def __call__(self, text):
+        try:
+            parse_tree = self.parser.parse(text.replace(",", ""))
+            return self.transformer.transform(parse_tree)
+        except Exception:
+            return None
+
+
+text_to_number = TextToNumber()
+# -
+
 # # Plot the number of participants
 
 # ## Load the data
@@ -23,7 +126,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from annotutils import database
-from participants_demographics import _reading
 
 
 connection = database.get_database_connection()
@@ -32,14 +134,14 @@ select_labels = [
     "N included",
     "n_participants",
     "n_participants_total",
-    "N_Total"
+    "N_Total",
 ]
 
 df = pd.read_sql(
     """
     SELECT * FROM  detailed_annotation
         WHERE label_name in ("N included", "n_participants", "n_participants_total", "N_Total");
-    
+
     """,
     connection,
 )
@@ -48,55 +150,35 @@ df.head()
 # -
 
 # ## Convert the annotated text to integers
-# this is not the best way, I think. 
+# this is not the best way, I think.
 
 # +
 
 
-class TextToNumber:
-    def __init__(self):
-        self.parser = _reading._get_parser(
-            start="number", ambiguity="resolve", grammar="numbers_grammar"
-        )
-        self.transformer = _reading.ParticipantsTransformer(0)
-
-    def __call__(self, text):
-        try:
-            parse_tree = self.parser.parse(text)
-            return self.transformer.transform(parse_tree).value
-        except Exception:
-            return None
-text_to_num = TextToNumber()
-
 for i, row in df.iterrows():
-    if row['extra_data']:
-        n_int = text_to_num(row['extra_data'])
-    else:
-        n_int = text_to_num(row['selected_text'])
+    try:
+        n_int = int(row["extra_data"])
+    except (TypeError, ValueError):
+        n_int = text_to_number(row["selected_text"])
     df.loc[i, "n_ptp_int"] = n_int
-    
-df.dropna(subset=['n_ptp_int'], inplace=True)
-df['n_ptp_int'] = df['n_ptp_int'].astype(int)
+
+df.dropna(subset=["n_ptp_int"], inplace=True)
+df["n_ptp_int"] = df["n_ptp_int"].astype(int)
 # -
 
 df.head()
 
 # ## Frequency of differt sample sizes
 
-fig, ax = plt.subplots(figsize=(10,5))
-df['n_ptp_int'].hist(bins=100)
-ax.set_ylabel('Number of papers')
-ax.set_xlabel('Number of participants')
+fig, ax = plt.subplots(figsize=(10, 5))
+df["n_ptp_int"].hist(bins=100)
+ax.set_ylabel("Number of papers")
+ax.set_xlabel("Number of participants")
 
 # ## Sample sizes over time
 
 fig, ax = plt.subplots()
-df.plot.scatter(x='publication_year', y='n_ptp_int', ax=ax, alpha=.3)
+df.plot.scatter(x="publication_year", y="n_ptp_int", ax=ax, alpha=0.3)
 
 fig, ax = plt.subplots()
-sns.lineplot(
-    data = df,
-    x="publication_year",
-    y="n_ptp_int",
-    ax=ax
-)
+sns.lineplot(data=df, x="publication_year", y="n_ptp_int", ax=ax)
