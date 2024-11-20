a5e21c63ce935e89c0e9c89f51d97987e60b2ab7
Alejandro de la Vega <aleph4@gmail.com>
2024-11-20 17:45:38 -0600

    Remove ids.json

--- scripts/checkout_docs.py
+++ scripts/checkout_docs.py
@@ -7,55 +7,57 @@ import pathlib
 import json
 import argparse
 import hashlib 
+from collections import defaultdict
+from labelrepo import database
 
+connection = database.get_database_connection()
 doc_path = pathlib.Path('documents')
 
+
+def _load_md5s(project_name):
+    """ Loads ids annotated in the project """
+    # Get all ids for each project
+    q = f"""SELECT project_name, pmcid, pmid, doc_md5 FROM
+                                detailed_annotation
+                                WHERE project_name = '{project_name}'
+                                """
+    cur = connection.execute(q)
+
+    # Write out pmcids for each project
+    md5s = defaultdict(set)
+    for row in cur.fetchall():
+        if row['pmcid']:
+            _id = f"pmcid_{row['pmcid']}"
+        else:
+            _id = f"pmid_{row['pmid']}"
+
+        md5s[_id].add(row['doc_md5'])
+
+    # Set to list
+    md5s = {k: list(v) for k, v in md5s.items()}
+
+    return md5s
+
+
 def checkout_docs(project_name, batch_size=200):
-
     project_dir = pathlib.Path('projects') / project_name
-    ids_file = project_dir / 'ids.json'
-    with open(ids_file, 'r') as f:
-        ids = json.load(f)
-
-    # Make a flat list of all the ids
-    all_ids = []
-    for id_type, id_value in ids.items():
-        all_ids.extend([f"{id_type}_{id}" for id in id_value])
-
-    # Load annotations in project
-    annotations = list(project_dir.glob('annotations/*.jsonl'))
-    # Make md5 checksums of the annotations
-    ann_md5s = {}
-    for annotation_file in annotations:
-        with open(annotation_file, 'r') as f:
-            for line in f:
-                ann_info = json.loads(line)
-
-                if isinstance(ann_info["metadata"], str):
-                    ann_info["metadata"] = json.loads(ann_info["metadata"])
-
-                if 'pmcid' in ann_info['metadata']:
-                    id = f"pmcid_{ann_info['metadata']['pmcid']}"
-                elif 'pmid' in ann_info['metadata']:
-                    id = f"pmid_{ann_info['metadata']['pmid']}"
-
-                ann_md5s[id] = ann_info['utf8_text_md5_checksum']
+    all_md5s = _load_md5s(project_name)
 
     # Load documents from central repository
     docs = []
-    for id in all_ids:
+    for id, ann_md5s in all_md5s.items():
         doc_file = doc_path / f"{id}.jsonl"
         if doc_file.exists():
             with open(doc_file, 'r') as f:
                 for line in f:
                     doc = json.loads(line)
-                    if id in ann_md5s:
-                        if ann_md5s[id] == hashlib.md5(doc["text"].encode("utf-8")).hexdigest():
-                            docs.append(doc)
-                            break
+                    doc_md5 = hashlib.md5(doc["text"].encode("utf-8")).hexdigest()
+                    if doc_md5 in ann_md5s:
+                        docs.append(doc)
+
 
     # Write the documents to the project directory
-    docs_dir = project_dir / '_documents'
+    docs_dir = project_dir / 'documents'
     docs_dir.mkdir(exist_ok=True)
 
     for i in range(0, len(docs), batch_size):
