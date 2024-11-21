f59a04c05d7bf1827752c262493057d5a8656b9b
Alejandro de la Vega <aleph4@gmail.com>
2024-11-20 18:26:37 -0600

    Add script to checkin docs centrally

---
+++ scripts/checkin_docs.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# This script is used to centralize the documents in the projects
+# Documents that are stores in {project_name}/documents/ and annotated are
+# copies to the central location in documents/
+
+from labelrepo.documents import checkin_docs
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--project', help='Project name to centralize')
+    parser.add_argument('--delete', action='store_true', help='Delete local project documents')
+    args = parser.parse_args()
+
+    checkin_docs(args.project, args.delete)
